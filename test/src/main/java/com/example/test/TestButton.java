cd9864e4e8866a1e5aceab20fb0bf03bceecb96c
zhangswings <beforenight@163.com>
2015-11-16 17:48:20 +0800

    View测试modules

---
+++ test/src/main/java/com/example/test/TestButton.java
@@ -0,0 +1,73 @@
+package com.example.test;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+import android.widget.TextView;
+
+import com.nineoldandroids.view.ViewHelper;
+
+/**
+ * Created by swings on 2015-11-16.
+ * 1.width = right-left
+ * 2.height = bottom-top
+ */
+public class TestButton extends TextView {
+
+    private static final String TAG = "TestButton";
+    private int mScaledTouchSlop;
+    // 分别记录上次滑动的坐标
+    private int mLastX = 0;
+    private int mLastY = 0;
+
+    public TestButton(Context context) {
+        this(context, null);
+    }
+
+    public TestButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        mScaledTouchSlop = ViewConfiguration.get(getContext()).getScaledTouchSlop();
+        Log.d(TAG, "sts： " + mScaledTouchSlop);
+    }
+
+    public TestButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getRawX();
+        int y = (int) event.getRawY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: {
+                int deltaX = x - mLastX;
+                int deltaY = y - mLastY;
+                Log.d(TAG, "move, deltaX:" + deltaX + " deltaY:" + deltaY);
+                int translationX = (int) ViewHelper.getTranslationX(this) + deltaX;
+                int translationY = (int) ViewHelper.getTranslationY(this) + deltaY;
+                ViewHelper.setTranslationX(this, translationX);
+                ViewHelper.setTranslationY(this, translationY);
+                break;
+            }
+            case MotionEvent.ACTION_UP: {
+                break;
+            }
+            default:
+                break;
+        }
+        mLastX=x;
+        mLastY=y;
+        return true;
+
+    }
+}
---
+++ test/src/main/java/com/example/test/User.java
@@ -0,0 +1,63 @@
+package com.example.test;
+
+/**
+ * Created by swings on 2015-11-16.
+ */
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.Serializable;
+
+public class User implements Parcelable, Serializable {
+    private static final long serialVersionUID = 519067123721295773L;
+
+    public int userId;
+    public String userName;
+    public boolean isMale;
+
+
+    public User() {
+    }
+
+    public User(int userId, String userName, boolean isMale) {
+        this.userId = userId;
+        this.userName = userName;
+        this.isMale = isMale;
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeInt(userId);
+        out.writeString(userName);
+        out.writeInt(isMale ? 1 : 0);
+    }
+
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    private User(Parcel in) {
+        userId = in.readInt();
+        userName = in.readString();
+        isMale = in.readInt() == 1;
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "User:{userId:%s, userName:%s, isMale:%s}",
+                userId, userName, isMale);
+    }
+
+}
