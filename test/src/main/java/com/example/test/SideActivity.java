2470928e75f1cfd7e747146cf1a98ab757e0d540
zhangswings <beforenight@163.com>
2015-12-14 15:50:19 +0800

    SlideAndDragListView 一个可以左右滑动item和拖放item的ListView。 https://github.com/yydcdut/SlideAndDragListView/blob/master/CHINESE.md

---
+++ test/src/main/java/com/example/test/SideActivity.java
@@ -0,0 +1,190 @@
+package com.example.test;
+
+import android.content.pm.ApplicationInfo;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.yydcdut.sdlv.Menu;
+import com.yydcdut.sdlv.MenuItem;
+import com.yydcdut.sdlv.SlideAndDragListView;
+
+import java.util.List;
+
+/**
+ * Created by yuyidong on 15/7/31.
+ */
+public class SideActivity extends AppCompatActivity implements SlideAndDragListView.OnListItemLongClickListener,
+        SlideAndDragListView.OnDragListener, SlideAndDragListView.OnSlideListener,
+        SlideAndDragListView.OnListItemClickListener, SlideAndDragListView.OnMenuItemClickListener,
+        SlideAndDragListView.OnItemDeleteListener {
+
+    private Menu mMenu;
+    private List<ApplicationInfo> mAppList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_side);
+        initData();
+        initMenu();
+        initUiAndListener();
+    }
+
+    public void initData() {
+        mAppList = getPackageManager().getInstalledApplications(0);
+    }
+
+    public void initMenu() {
+        mMenu = new Menu((int) getResources().getDimension(R.dimen.slv_item_height), new ColorDrawable(Color.LTGRAY), true);
+        mMenu.addItem(new MenuItem.Builder().setWidth((int) getResources().getDimension(R.dimen.slv_item_bg_btn_width) * 2)
+                .setBackground(new ColorDrawable(Color.RED))
+                .setText("One")
+                .setTextColor(Color.GRAY)
+                .setTextSize((int) getResources().getDimension(R.dimen.txt_size))
+                .build());
+        mMenu.addItem(new MenuItem.Builder().setWidth((int) getResources().getDimension(R.dimen.slv_item_bg_btn_width))
+                .setBackground(new ColorDrawable(Color.GREEN))
+                .setText("Two")
+                .setTextColor(Color.BLACK)
+                .setTextSize((int) getResources().getDimension(R.dimen.txt_size))
+                .build());
+        mMenu.addItem(new MenuItem.Builder().setWidth((int) getResources().getDimension(R.dimen.slv_item_bg_btn_width) + 30)
+                .setBackground(new ColorDrawable(Color.BLUE))
+                .setText("Three")
+                .setDirection(MenuItem.DIRECTION_RIGHT)
+                .setTextColor(Color.BLACK)
+                .setTextSize((int) getResources().getDimension(R.dimen.txt_size))
+                .build());
+        mMenu.addItem(new MenuItem.Builder().setWidth((int) getResources().getDimension(R.dimen.slv_item_bg_btn_width_img))
+                .setBackground(new ColorDrawable(Color.BLACK))
+                .setDirection(MenuItem.DIRECTION_RIGHT)
+                .setIcon(getResources().getDrawable(R.mipmap.ic_launcher))
+                .build());
+    }
+
+    public void initUiAndListener() {
+        SlideAndDragListView listView = (SlideAndDragListView) findViewById(R.id.lv_edit);
+        listView.setMenu(mMenu);
+        listView.setAdapter(mAdapter);
+        listView.setOnListItemLongClickListener(this);
+        listView.setOnDragListener(this, mAppList);
+        listView.setOnListItemClickListener(this);
+        listView.setOnSlideListener(this);
+        listView.setOnMenuItemClickListener(this);
+        listView.setOnItemDeleteListener(this);
+    }
+
+    private BaseAdapter mAdapter = new BaseAdapter() {
+        @Override
+        public int getCount() {
+            return mAppList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mAppList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            CustomViewHolder cvh;
+            if (convertView == null) {
+                cvh = new CustomViewHolder();
+                convertView = LayoutInflater.from(SideActivity.this).inflate(R.layout.item_custom, null);
+                cvh.imgLogo = (ImageView) convertView.findViewById(R.id.img_item_edit);
+                cvh.txtName = (TextView) convertView.findViewById(R.id.txt_item_edit);
+                cvh.imgLogo2 = (ImageView) convertView.findViewById(R.id.img_item_edit2);
+                convertView.setTag(cvh);
+            } else {
+                cvh = (CustomViewHolder) convertView.getTag();
+            }
+            ApplicationInfo item = (ApplicationInfo) this.getItem(position);
+            cvh.txtName.setText(item.loadLabel(getPackageManager()));
+            cvh.imgLogo.setImageDrawable(item.loadIcon(getPackageManager()));
+            cvh.imgLogo2.setImageDrawable(item.loadIcon(getPackageManager()));
+            return convertView;
+        }
+
+        class CustomViewHolder {
+            public ImageView imgLogo;
+            public TextView txtName;
+            public ImageView imgLogo2;
+        }
+    };
+
+    @Override
+    public void onListItemLongClick(View view, int position) {
+//        Toast.makeText(DemoActivity.this, "onItemLongClick   position--->" + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDragViewStart(int position) {
+//        Toast.makeText(DemoActivity.this, "onDragViewStart   position--->" + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDragViewMoving(int position) {
+//        Toast.makeText(DemoActivity.this, "onDragViewMoving   position--->" + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDragViewDown(int position) {
+//        Toast.makeText(DemoActivity.this, "onDragViewDown   position--->" + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onListItemClick(View v, int position) {
+//        Toast.makeText(DemoActivity.this, "onItemClick   position--->" + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSlideOpen(View view, View parentView, int position, int direction) {
+//        Toast.makeText(DemoActivity.this, "onSlideOpen   position--->" + position + "  direction--->" + direction, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSlideClose(View view, View parentView, int position, int direction) {
+//        Toast.makeText(DemoActivity.this, "onSlideClose   position--->" + position + "  direction--->" + direction, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public int onMenuItemClick(View v, int itemPosition, int buttonPosition, int direction) {
+        switch (direction) {
+            case MenuItem.DIRECTION_LEFT:
+                switch (buttonPosition) {
+                    case 0:
+                        return Menu.ITEM_NOTHING;
+                    case 1:
+                        return Menu.ITEM_SCROLL_BACK;
+                }
+                break;
+            case MenuItem.DIRECTION_RIGHT:
+                switch (buttonPosition) {
+                    case 0:
+                        return Menu.ITEM_SCROLL_BACK;
+                    case 1:
+                        return Menu.ITEM_DELETE_FROM_BOTTOM_TO_TOP;
+                }
+        }
+        return Menu.ITEM_NOTHING;
+    }
+
+    @Override
+    public void onItemDelete(View view, int position) {
+        mAppList.remove(position);
+        mAdapter.notifyDataSetChanged();
+    }
+}
